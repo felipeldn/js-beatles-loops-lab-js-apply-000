3143152e391faaea1ad539d6b71c5372adb06515
Felipe <felipeldn91@gmail.com>
2019-04-30 08:14:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-
-function theBeatlesPlay(musicians, instruments) {
-    var allMusicians = [];
-    for (var i=0; i < musicians.length; i++) {
-        allMusicians.push(musicians[i] + 'plays' + instruments[i]);
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
     }
-    return allMusicians;
+    return allPlayers;
 }
 
 function johnLennonFacts() {
-    var facts = ['He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and boy scout', 'He hated the sound of his own voice'];
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
     var newFacts = [];
     var i = 0;
     while (i < facts.length) {
